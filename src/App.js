1e1a1a02cf6e082df76fb725556b7093364585e4
Loskutov Vladimir <vmloskutov@gmail.com>
2019-05-02 19:54:02 +0300

    API added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import Info from "./components/info";
 import Form from "./components/form";
 import Weather from "./components/weather";
 
+const API_KEY ="31d8cd5bb9c6aec41d284a3c7b901c23"
+
 class App extends React.Component {
+
+  gettingWeather = async () => {
+    const api_url = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
