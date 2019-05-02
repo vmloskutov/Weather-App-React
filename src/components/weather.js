e544dd16cf30499c4242ea4c717b294b1fd8e9dc
Loskutov Vladimir <vmloskutov@gmail.com>
2019-05-02 19:50:20 +0300

    API added

---
+++ src/components/weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Weather  extends React.Component {
+  render (){
+    return (
+      <div>Компонент с погодной информацей</div>
+    );
+  }
+}
+
+export default Weather;
